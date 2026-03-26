d7182d46eb74a72128424ed5b06937974524414c
dharmendra <yaduvanshi.dhanno@gmail.com>
2026-03-26 08:33:14 +0530

    feat: BE: drizzle and db setup

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,17 +1,21 @@
 import dotenv from 'dotenv';
 
 import express from "express";
-import type {Request, Response} from "express"
+import type {Request, Response} from "express";
+
+import { drizzle } from 'drizzle-orm/neon-http';
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-console.log(PORT)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.get("/", (req:Request, res: Response) => {
   res.send({
